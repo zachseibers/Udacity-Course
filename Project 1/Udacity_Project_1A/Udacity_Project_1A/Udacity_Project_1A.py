89293b28993cd621647d1a6ae1de12267e955a24
zachseibers <zachseibers@gmail.com>
2017-02-19 23:42:57 -0500

    Added time delay and embedded into while loop

--- Project 1/Udacity_Project_1A/Udacity_Project_1A/Udacity_Project_1A.py
+++ Project 1/Udacity_Project_1A/Udacity_Project_1A/Udacity_Project_1A.py
@@ -5,6 +5,15 @@
 #2) Open Browser to play song
 #3) Repeat n number of times
 
-import webbrowser
+import webbrowser,time
 
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=NUC2EQvdzmY")
+total_breaks = 3
+break_count = 1
+
+print("This program started on "+time.ctime())
+while(break_count <= total_breaks):
+    time.sleep(5)
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=NUC2EQvdzmY")
+    print("This is break number",break_count)
+    break_count = break_count + 1
+print('good job')
