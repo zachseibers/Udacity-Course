19cf5a753db35fd6a04d37abe692dd4dfc45d472
zachseibers <zachseibers@gmail.com>
2017-02-19 23:19:01 -0500

    Outlined steps of program

--- Project 1/Udacity_Project_1A/Udacity_Project_1A/Udacity_Project_1A.py
+++ Project 1/Udacity_Project_1A/Udacity_Project_1A/Udacity_Project_1A.py
@@ -1,1 +1,8 @@
-#Project 1 of Udacity Course
+#Project 1 of Udacity Course - Take a Break
+
+#Key Steps
+#1) Initiate, and wait 2 hours
+#2) Open Browser to play song
+#3) Repeat n number of times
+
+
